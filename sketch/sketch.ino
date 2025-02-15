f21b5e0badb03da4706741e241d3a32bad873876
Erik <erikd.berg@me.com>
2025-02-14 22:59:10 -0600

    Arduino Sketch

--- sketch/sketch.ino
+++ sketch/sketch.ino
@@ -1,52 +1,81 @@
 #include <FastLED.h>
 
-#define LED_PIN     6       // Data Pin
+#define LED_PIN     6       // Data pin
 #define NUM_LEDS    600     // LEDs
 #define TRACK_LENGTH 200.0  // 200m track
 
 CRGB leds[NUM_LEDS];
 
-// Function to calculate LED speed and control pacing
 void runPacer(CRGB color, float distance, int minutes, int seconds, int intervals, int delayMs) {
-    // Convert duration to total seconds
     float totalTime = (minutes * 60.0) + seconds;
-    
-    // Calculate speed in meters per second
-    float speed = distance / totalTime;  // m/s
-    
-    // Calculate LED position per update
+    float speed = distance / totalTime;  
     float metersPerLED = TRACK_LENGTH / NUM_LEDS;
-    float ledSpeed = speed / metersPerLED;  // LEDs per second
-    
-    int intervalTime = totalTime / intervals;  // Time for each interval
+    float ledSpeed = speed / metersPerLED;  
+
+    int intervalTime = totalTime / intervals;  
 
     Serial.print("Speed: "); Serial.print(speed); Serial.println(" m/s");
     Serial.print("LED Speed: "); Serial.print(ledSpeed); Serial.println(" LEDs/s");
 
-    // Run pacing animation
     for (int i = 0; i < intervals; i++) {
         for (int pos = 0; pos < NUM_LEDS; pos++) {
-            // Clear LEDs
             FastLED.clear();
-            // Set current LED
             leds[pos] = color;
             FastLED.show();
-            delay((1000 / ledSpeed));  // Adjust delay based on calculated LED speed
+            delay((1000 / ledSpeed));  
         }
-        delay(delayMs);  // Delay between intervals
+        delay(delayMs);
+    }
+}
+
+void parseData(String input) {
+    int values[7];
+    int index = 0;
+    char *ptr = strtok((char*)input.c_str(), ",");
+    
+    while (ptr != NULL && index < 7) {
+        values[index++] = atoi(ptr);
+        ptr = strtok(NULL, ",");
+    }
+    
+    if (index == 7) {
+        CRGB color = CRGB(values[0], values[1], values[2]);
+        float distance = values[3];
+        int minutes = values[4];
+        int seconds = values[5];
+        int intervals = values[6];
+        int delayMs = values[7];
+
+        Serial.println("Received Data:");
+        Serial.print("Color: "); Serial.print(values[0]); Serial.print(", ");
+        Serial.print(values[1]); Serial.print(", "); Serial.println(values[2]);
+        Serial.print("Distance: "); Serial.println(distance);
+        Serial.print("Duration: "); Serial.print(minutes); Serial.print(":"); Serial.println(seconds);
+        Serial.print("Intervals: "); Serial.println(intervals);
+        Serial.print("Delay: "); Serial.println(delayMs);
+
+        runPacer(color, distance, minutes, seconds, intervals, delayMs);
+    } else {
+        Serial.println("Invalid data received!");
     }
 }
 
 void setup() {
     Serial.begin(115200);
+    Serial2.begin(115200);
+
     FastLED.addLeds<WS2812B, LED_PIN, GRB>(leds, NUM_LEDS);
     FastLED.clear();
     FastLED.show();
 
-    // Example usage: Red color, 200m, 2 minutes, 30 seconds, 3 intervals, 1000ms delay
-    runPacer(CRGB::Red, 200.0, 2, 30, 3, 1000);
+    Serial.println("Waiting for data...");
 }
 
 void loop() {
-    // Nothing in loop, function is called in setup for testing
+    if (Serial1.available()) {  
+        String data = Serial1.readStringUntil('\n');  
+        Serial.print("Received: ");
+        Serial.println(data);
+        parseData(data);
+    }
 }
